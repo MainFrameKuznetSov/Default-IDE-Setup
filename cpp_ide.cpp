7c015397a812ddfa129b397526089e78fdd758f1
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-01-29 19:14:35 +0530

    Update cpp_ide.cpp

--- cpp_ide.cpp
+++ cpp_ide.cpp
@@ -68,10 +68,10 @@ void solve()
 }
 int main() 
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t;
-	cin>>t;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
 	while(t--)
 	{
 	   solve();
