35623ceaf3c3bb34deb4452c078d4c6f021ba8e8
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-17 15:22:44 +0530

    Update cpp_ide.cpp

--- cpp_ide.cpp
+++ cpp_ide.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define MOD 1000000007
+#define pb push_back
+#define F first
+#define S second
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rrep(i,n) for(int i=n;i>=0;--i)
+#define lloop(i,a,b) for(int i=a;i<=b;++i)
+#define jloop(i,a,b,k) for(int i=a;i<=b;i+=k)
+#define vi vector<int>
+#define vll vector<long long>
+#define vpii vector<pair<int,int>>
 using namespace std;
-int BinarySearchDesc(vector<int> x,int t)
+int BinarySearch(vi x,int t)
 {
-    int l=0,r=x.size()-1,mid;
-    while(l<=r)
-    {
-        mid=l+(r-l)/2;
-        if(x[mid]==t)
-            return mid;
-        if(x[mid]>t)
-            l=mid+1;
-        else
-            r=mid-1;
-    }
-    return -1;
-}
-int BinarySearch(vector<int> x,int t)
-{
+    /*
+        For descending order,
+            replace r & l
+    */
     int l=0,r=x.size()-1,mid;
     while(l<=r)
     {
@@ -29,25 +33,6 @@ int BinarySearch(vector<int> x,int t)
             l=mid+1;
     }
     return -1;
-}
-vector<int> getSieve(int n)
-{
-    vector<bool> isPrime(n+1,1);
-    vector<int> getPrime;
-    isPrime[0]=0;
-    isPrime[1]=0;
-    for(int i=2;i*i<=n;i++)
-    {
-        if(isPrime[i])
-        for(int j=i*i;j<=n;j+=i)
-            isPrime[j]=0;
-    }
-    for(int i=2;i<=n;i++)
-    {
-        if(isPrime[i])
-            getPrime.push_back(i);
-    }
-    return getPrime;
 }
 long long exp(int a,int b)
 {
@@ -60,21 +45,34 @@ long long exp(int a,int b)
     else
         return a*exp(a*a,(b-1)/2);
 }
+ll selfPower(ll x,ll y,ll p) 
+{ 
+    ll res=1;
+    x=x%p;   
+    if(x==0) 
+        return 0;
+    while(y>0) 
+    { 
+        if(y&1) 
+            res=(res*x)%p; 
+        y=y>>1;
+        x=(x*x)%p; 
+    } 
+    return res; 
+}
 void solve()
 {
-    /*Your Code starts here*/
+    //Your Code starts here
     int n;
     cin>>n;
 }
 int main() 
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin>>t;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
 	while(t--)
-	{
 	   solve();
-	}
 	return 0;
 }
